40ee8ee897c95779cdd9da23d6aa560604c6409b
shaima <s.alghamdi@safcsp.org.sa>
2022-03-07 16:22:19 +0300

    1. Setting up Project Structure (Folders and Packages) 2. Implementing APIs and Models 3. Creating the repository 4. Implementing the Use Cases 5. Setting up dependency injection

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/domain/use_case/get_coin/GetCoinUseCase.kt
@@ -0,0 +1,32 @@
+package com.plcoding.cryptocurrencyappyt.domain.use_case.get_coin
+
+import com.plcoding.cryptocurrencyappyt.common.Resource
+import com.plcoding.cryptocurrencyappyt.data.remote.dto.toCoin
+import com.plcoding.cryptocurrencyappyt.data.remote.dto.toCoinDetail
+import com.plcoding.cryptocurrencyappyt.domain.model.Coin
+import com.plcoding.cryptocurrencyappyt.domain.model.CoinDetail
+import com.plcoding.cryptocurrencyappyt.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinUseCase @Inject constructor(
+    private val repository: CoinRepository
+    // use the repo interface easily replacable
+) {
+    // invoke the operator function
+    // a use case always have one public function to get the coin details
+    operator fun invoke(coinId: String): Flow<Resource<CoinDetail>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coin = repository.getCoinById(coinId).toCoinDetail()
+            emit(Resource.Success(coin))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured."))
+        } catch (e: IOException) {
+            emit(Resource.Error("Couldn't reach server. Check your internet connection."))
+        }
+    }
+}
